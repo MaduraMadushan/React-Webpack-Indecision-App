2570940a52d2736944f3583b3c31b8b74e35dcf1
MaduraMadushan <39506813+MaduraMadushan@users.noreply.github.com>
2019-01-03 16:13:39 +0530

    Indecision App

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 
-const template = <h1>Hello React</h1>;
-ReactDOM.render(template, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+  
